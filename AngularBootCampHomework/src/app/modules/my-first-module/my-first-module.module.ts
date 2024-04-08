151c2e912fe84c80d9fa1b8b55e562bc2fcad057
Ivan Ruiz <iruiz@apexsystems.com>
2024-04-07 22:29:40 -0600

    Complete menu

--- AngularBootCampHomework/src/app/modules/my-first-module/my-first-module.module.ts
+++ AngularBootCampHomework/src/app/modules/my-first-module/my-first-module.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
-import { MyFirstModuleRoutingModule } from './my-first-module-routing.module';
 import {MyFirstComponentComponent} from '../../components/my-first-component/my-first-component.component';
 
 const routes: Routes =[
@@ -15,8 +14,6 @@ const routes: Routes =[
   declarations: [],
   imports: [
     CommonModule,
-    MyFirstModuleRoutingModule, 
-    //MyFirstComponentComponent, 
     RouterModule.forChild(routes)
   ]
 })
